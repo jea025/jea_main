50320c45714cf3b1a5297194f1615796111960df
vdb3 <bogadovd@hotmail.com>
2024-12-05 14:44:53 -0300

    nav styles

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,131 +1,72 @@
 "use client";
-import React, { useState } from "react";
-import "./Navbar.css";
-import Home from "../Home/Home";
-import Nosotros from "../Nosotros/Nosotros";
-import Contacto from "../Contacto/Contacto";
-import Prensa from "../Prensa/Prensa";
-import Enlaces from "../Enlaces/Enlaces";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/logo.png";
-import ham from "../../public/ham.svg";
-
-// Definimos el tipo para las secciones
-type Section = "home" | "nosotros" | "contacto" | "prensa" | "enlaces";
+import { usePathname } from "next/navigation"; // Importar usePathname
 
 const Navbar: React.FC = () => {
-  // Estado para la sección activa, tipada como Section
-  const [activeSection, setActiveSection] = useState<Section>("home");
-
-  // Función para manejar la navegación entre secciones
-  const handleNavigation = (section: Section) => {
-    setActiveSection(section);
-    changeActiveBorder(section);
-  };
-
-  // Cambiar el borde visual de la sección activa
-  const changeActiveBorder = (section: Section) => {
-    clearBorder();
-    const element = document.getElementById(section);
-    if (element) {
-      element.classList.add("active");
-    }
-  };
-
-  // Limpiar la clase activa de los elementos
-  const clearBorder = () => {
-    const homeElement = document.getElementById("home");
-    if (homeElement) {
-      homeElement.classList.remove("homeStyle");
-    }
-    const buttons = document.querySelectorAll<HTMLButtonElement>(".navButton");
-    buttons.forEach((button) => {
-      button.classList.remove("active");
-    });
-  };
-
-
+  const pathname = usePathname(); 
+  const activeClass = "border-l-2 border-white text-white font-bold"; // Estilo para el enlace activo
+  const defaultClass = "text-white hover:text-gray-300"; // Estilo por defecto
 
   return (
-    <div className="navbarContainer">
-      <nav>
-        <Image className="margin" src={logo} alt="logo" />
-        <div className="navButtons">
+    <div className="w-full h-16">
+      <nav className="w-full bg-customCyan flex justify-between items-center">
+        <Image className="w-28 ml-8" src={logo} alt="logo" />
+        <div className="h-16 flex items-center w-full justify-end mr-14">
           <Link href={"/home"}>
-            <button id="home" className="navButton homeStyle">
+            <button
+              id="home"
+              className={`h-8 px-1 py-2 mx-4 my-1 ${
+                pathname === "/home" ? activeClass : defaultClass
+              }`}
+            >
               HOME
             </button>
           </Link>
           <Link href={"/nosotros"}>
-            <button id="nosotros" className="navButton">
+            <button
+              id="nosotros"
+              className={`h-8 px-1 py-2 mx-4 my-1 ${
+                pathname === "/nosotros" ? activeClass : defaultClass
+              }`}
+            >
               NOSOTROS
             </button>
           </Link>
           <Link href={"/contacto"}>
-            <button id="contacto" className="navButton">
+            <button
+              id="contacto"
+              className={`h-8 px-1 py-2 mx-4 my-1 ${
+                pathname === "/contacto" ? activeClass : defaultClass
+              }`}
+            >
               CONTACTO
             </button>
           </Link>
           <Link href={"/prensa"}>
-            <button id="prensa" className="navButton">
-              PRENSA
-            </button>
-          </Link>
-          <Link href={'/enlaces'}>
-            <button
-              id="enlaces"
-              className="navButton"
-            >
-              ENLACES
-            </button>
-          </Link>
-        </div>
-        <div className="hamNav margin">
-          <input className="hamCheck" type="checkbox" id="ham" />
-          <label htmlFor="ham">
-            <Image className="hamSvg" src={ham} alt="menu" />
-          </label>
-          <div className="hamButtons">
-            <button
-              id="home"
-              className="hamButton"
-              onClick={() => handleNavigation("home")}
-            >
-              HOME
-            </button>
-            <button
-              id="nosotros"
-              className="hamButton"
-              onClick={() => handleNavigation("nosotros")}
-            >
-              NOSOTROS
-            </button>
-            <button
-              id="contacto"
-              className="hamButton"
-              onClick={() => handleNavigation("contacto")}
-            >
-              CONTACTO
-            </button>
             <button
               id="prensa"
-              className="hamButton"
-              onClick={() => handleNavigation("prensa")}
+              className={`h-8 px-1 py-2 mx-4 my-1 ${
+                pathname === "/prensa" ? activeClass : defaultClass
+              }`}
             >
               PRENSA
             </button>
+          </Link>
+          <Link href={"/enlaces"}>
             <button
               id="enlaces"
-              className="hamButton"
-              onClick={() => handleNavigation("enlaces")}
+              className={`h-8 px-1 py-2 mx-4 my-1 ${
+                pathname === "/enlaces" ? activeClass : defaultClass
+              }`}
             >
               ENLACES
             </button>
-          </div>
+          </Link>
         </div>
       </nav>
-
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        customCyan: 'rgb(32, 170, 182)',
       },
     },
   },
