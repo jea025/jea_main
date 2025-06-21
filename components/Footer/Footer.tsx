c8ebde1158a79c6a1be7c755993ff31c57b00983
VDB3 <etcde89@gmail.com>
2025-06-21 14:09:02 -0300

    footer disabled

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,9 +5,9 @@ import Enlaces from "../Enlaces/Enlaces";
 export default function Footer() {
   return (
     <div className="w-full h-1/3 flex flex-col bg-gradient-to-r from-cyan-700 to-customCyan ">
-
       <div className="w-full h-1 bg-white mt-2 mb-2 border-r-1"></div>
       <div className="w-full flex flex-row items-center justify-center ">
+        {/*
         <p className="text-center select-none text-white mr-2 font-bold h-full">
           Con el apoyo de
         </p>
@@ -21,6 +21,10 @@ export default function Footer() {
         <p className="text-center select-none font-bold text-white mt-28">
           Patrocinador oficial de Jóvenes en Acción
         </p>
+
+        TODO: poner los sponsors
+
+          */} 
       </div>
 
       <div className="w-full h-1 bg-cyan-700 mt-2 mb-2 border-r-1"></div>
@@ -35,7 +39,6 @@ export default function Footer() {
           </p>
         </h3>
       </div>
-
     </div>
   );
 }
