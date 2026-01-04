2126fa1fc6d5a44c478db7881536b1d976ba7489
Nicolás Monzón <nmonzon.2022@gmail.com>
2026-01-03 23:46:55 -0300

    Intento de sacar el logo de Next

--- next.config.ts
+++ next.config.ts
@@ -14,11 +14,23 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     qualities: [75, 85, 90, 95, 100],
   },
   
+  async headers() {
+    return [
+      {
+        source: '/:path*.(jpg|jpeg|png|gif|webp|avif|svg)',
+        headers: [
+          {
+            key: 'Content-Disposition',
+            value: 'inline',
+          },
+        ],
+      },
+    ];
+  },
+
   async redirects() {
     return [
       {
@@ -33,8 +45,14 @@ const nextConfig: NextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
 
+  devIndicators: {
+    buildActivity: false,
+    buildActivityPosition: 'bottom-right',
+  },
+
   experimental: {
     optimizePackageImports: ['react-bootstrap', 'react-icons'],
+    devOverlay: false,
   },
 };
 
